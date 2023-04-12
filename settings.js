28768ba44b80170e136443ae8525edfd8c86d493
Andyganteng <nekiam56@gmail.com>
2023-04-12 13:08:01 +0700

    scbot

---
+++ settings.js
@@ -0,0 +1,905 @@
+//Ubah Image/Thumbnail di folder maslent/pplent/maslent.png (Cari fotomu Ubah namnya ganti jadi maslent.png trus di gantu make pp mu yang baru
+
+const fs = require('fs')
+const chalk = require('chalk')
+global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+global.doc2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+global.doc3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+global.doc4 = 'application/zip'
+global.doc5 = 'application/pdf'
+global.doc6 = 'application/vnd.android.package-archive'
+//==============================\
+autoread = true //jangan di ubah
+global.footer = 'Andy'//ganti
+global.pulsa = "083154896636"//ganti
+global.gopay = "083154896636"//ganti
+global.dana = "6288221735230"//ganti
+global.paypal = "---" // biarin aja kalau gaada
+global.shopay = "083154896636" // biarin aja kalau gaada
+global.saweria = "---" // biarin aja kalau gaada
+global.sociabus = "---" // biarin aja kalau gaada
+global.bni = "---" // biarin aja kalau gaada
+global.bri = "---" // biarin aja kalau gaada
+global.bankjatim = "---" // biarin aja kalau gaada
+global.jago = "---" // biarin aja kalau gaada
+global.neobank = "---" // biarin aja kalau gaada
+global.packname = 'Andy'//ganti
+global.footer = '©Andy'//ganti
+global.ovo = "---"//ganti
+global.ownerlen = "Andy"//Ubah
+global.owner = ['6283154896636'] //ubh nomor owner hp
+global.ownername = "*Andy*" //ubah
+global.ytname = "YT: Anndy596"//ubah
+global.socialm = "IG: Andy_vgabut"//ubah
+global.location = "Indonesia, JawaTengah, Tegal"//ubah
+global.ownernomer = "6283154896636"//ubah nomorhp owner
+global.premium = ['6283154896636']//ubah nomor hp premium
+global.nomorcs = '6283154896636'
+global.botname = 'Ken Kaneki' //ubah
+global.linkz = "https://chat.whatsapp.com/Bky2I0u2deR0VZtW2gKmRC"//ubah
+global.websitex = "https://youtube.com/@Anndy596"//ubah
+global.botscript = '-'
+global.themeemoji = "😿"//ubah
+global.packname = "Sticker By Andy"//ubah
+global.author = "Ken Kaneki 𝘽𝙮 Andy"//ubah
+global.wm = "Ken Kaneki"//ubah
+global.sessionName = 'session'
+global.prefa = ['','!','.','#','&']
+global.sp = ''
+global.mess = {
+    success: 'Apa?!', //ubah...
+    admin: 'This feature could be used by admins only!',
+    botAdmin: 'Bot Must Be Admin First!',
+    premime: 'Premium Special Features If You Want to Register Type Rent',
+    owner: 'This feature could be used by owner only', 
+    group: 'Features Used Only For Groups!',
+    private: 'Features Used Only For Private Chat!',
+    bot: 'This feature could be used by bot only',
+    wait: 'In process...',
+    linkm: 'Where is the link?',
+    endLimit: 'Upss Limit Kamu sudah habis..Tunggu Waktu menunjukan Pukul 12:00 Limit Anda otomatis akan Di reset!!!',
+    nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
+}
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.thum = fs.readFileSync("./Maslent/pplent/maslent.jpg")
+global.log0 = fs.readFileSync("./Maslent/pplent/maslent.jpg")
+global.err4r = fs.readFileSync("./Maslent/pplent/maslent.jpg")
+global.thumb = fs.readFileSync("./Maslent/pplent/maslent.jpg")
+
+//SETTING AJA SUKA SUKA ASAL PERHATIIN TANDA PETIK MAUPUN KURUNG
+//Jagan lupa menunya di tambhin ya Soalnya saya capek wkkw
+global.allmenu = `
+
+
+𝚁𝚢𝚞𝚞𝚓𝚒//𝙰𝚒𝚜𝚊𝚔𝚊
+
+
+openai < query >
+simi < query >
+dalle < query >
+dalle2 < Query >
+voiceopenai < query >
+voicesimi < query >
+chichi < query >
+dokter < query >
+dukun < query >
+antikaryawankerja
+aesthetic
+anime
+bike
+blackpink
+boneka
+car
+chinese
+coffee
+cosplay
+couplepicture
+doggo
+gamer
+hacking
+hijab
+indo
+japanese
+kayes
+korean
+kpop
+lizard
+malay
+meow
+notnot
+ppcouple2
+profilepicture
+pubg
+randomboy
+randomgirl
+rose
+ryujin
+teknologi
+thai
+ulzzangboy
+ulzzanggirl
+wallml
+wallpapercyber
+wallpaperanimex
+wallpaperislami
+wallpapermeme
+wallpapergamer
+wallphone
+woof
+ceritahoror
+cerpen-sejarah
+cerpen-sedih
+cerpen-sastra
+cerpen-romantis
+cerpen-rohani
+cerpen-rindu
+cerpen-remaja
+cerpen-ramadhan
+cerpen-petualangan
+cerpen-persahabatan
+cerpen-perpisahan
+cerpen-perjuangan
+cerpen-penyesalan
+cerpen-pengorbanan
+cerpen-pengalaman
+cerpen-pendidikan
+cerpen-penantian
+cerpen-patahhati
+cerpen-olahraga
+cerpen-nasionalisme
+cerpen-nasihat
+cerpen-motifasi
+cerpen-misteri
+cerpen-mengharukan
+cerpen-malaysia
+cerpen-liburan
+cerpen-kristen
+cerpen-korea
+cerpen-kisahnyata
+cerpen-keluarga
+cerpen-kehidupan
+cerpen-jepang 
+cerpen-inspirstif
+cerpen-gokil
+cerpen-galau
+cerpen-cintasejati
+cerpen-cintasegitiga
+cerpen-cintasedih
+cerpen-cintaromantis
+cerpen-cintapertama
+cerpen-cintaislami
+cerpen-cinta
+cerpen-budaya
+cerpen-bahasajawa
+cerpen-bahasasunda
+cerpen-bahasainggris
+cerpen-bahasadaerah
+cerpen-anak
+attp < text >
+attp2 < text >
+darahmaker < text >
+3dmaker < text >
+tembokmaker < text >
+kertasterbakar < text >
+ultahmaker < text >
+cardidulfitri < text >
+kueultahmaker < text >
+amongusmaker < text >
+dotamaker < text >
+fpsmaker < text >
+mlmaker < text >
+mlmaker2 < text >
+hengkermaker < text >
+neonlovemaker < text >
+ledmaker < text >
+firaunmaker < text >
+3draimbowmaker < text >
+summermaker < text >
+summer2maker < text >
+thunder2maker < text >
+lightglowmaker < text >
+batmanmaker < text >
+bussinesmaker < text >
+ohnomaker < text >
+tololsert < text >
+bucinsert < text >
+jadianime < text >
+nulis < text >
+candy < text >
+blackpinkneon < text >
+deepsea < text >
+scifi < text >
+fiction < text >
+berry < text >
+papercut <text>
+transformer <text>
+neondevil <text>
+3davengers <text>
+3dstone <text>
+3dstone2 <text>
+summertime <text>
+thunder <text>
+window <text>
+graffiti <text>
+graffitibike <text>
+pornhub <text>
+glitch <text>
+blackpinkart <text>
+glitch2 <text>
+glitch3 <text>
+3dspace <text>
+lion <text>
+3dneon <text>
+greenneon <text>
+bokeh <text>
+holographic <text>
+bear <text>
+wolf <text>
+joker <text>
+dropwater <text>
+dropwater2 <text>
+thewall <text>
+neonlight <text>
+natural <text>
+carbon <text>
+pencil <text>
+blackpink2 <text>
+neon <text>
+neonlight2 <text>
+toxic <text>
+strawberry <text>
+discovery <text>
+1917 <text>
+lion2 <text>
+papercut < text >
+transformer < text >
+neondevil < text >
+3davengers < text >
+3dstone < text >
+3dstone2 < text >
+summertime < text >
+thunder < text >
+graffiti < text >
+graffitibike < text >
+pornhub < text >
+glitch < text >
+blackpinkart < text >
+glitch2 < text >
+glitch3 < text >
+3dspace < text >
+lion < text >
+3dneon < text >
+greenneon < text >
+bokeh < text >
+holographic < text >
+bear < text >
+wolf < text >
+joker < text >
+dropwater < text >
+dropwater2 < text >
+thewall < text >
+neonlight < text >
+natural < text >
+carbon < text >
+pencil < text >
+blackpink2 < text >
+neon < text >
+neonlight2 < text >
+toxic < text >
+strawberry < text >
+discovery < text >
+1917 < text >
+sci_fi < text >
+ancient < text >
+fabric < text >
+hoorror < text >
+whitebear < text >
+juice < text >
+batman2 < text >
+multicolor < text >
+wonderful < text >
+sketch < text >
+marvel < text >
+foggy < text >
+writing < text >
+halloweenfire < text >
+halloween < text >
+watercolor < text >
+classic < text >
+animawall [query]
+animawall2 [query]
+animbite
+animblush
+animbonk
+animbully
+animcringe
+animcry
+animcuddle
+animdance
+animeawoo
+animecuddle
+animecry
+animeglomp
+animehandhold
+animehappy
+animehighfive
+animekiss
+animelick
+animemegumin
+animeneko
+animenom
+animepat
+animepoke
+animeslap
+animesmile
+animesmug
+animeslap
+animesmug
+animewave
+animewlp
+animewink
+animeyeet
+avatar
+awoo
+bully
+couplepp
+cringe
+cry
+dance
+feed
+foxgirl
+gecg
+gura
+handhold
+happy
+highfive
+hug
+husbu
+kiss
+lick
+loli-waifu
+neko
+neko2
+nom
+pat
+randomanime
+randomart
+randomawloli
+randombite
+randombts
+randombully
+shinobu
+shota
+slap
+smile
+smug
+tickle
+waifu
+waifu2
+wave
+artinama
+cekmati
+suitwithbot
+puisi
+namarandom
+quotesimage
+quotesbucin
+quoteskatabucin
+quotesanimebijak
+truth
+dare
+tictactoe
+delttt
+tebak [lagu]
+math [mode]
+suitpvp [tag]
+akinator
+tebaklagu
+say [text]
+define [text]
+how [text]
+when [text]
+where [text]
+is [text]
+what [text]
+can [text]
+rate [text]
+coolcheck [tag]
+stupidcheck [tag]
+waifucheck [tag]
+evilcheck [tag]
+dogcheck [tag]
+hotcheck [tag]
+smartcheck [tag]
+uncleancheck [tag]
+greatcheck [tag]
+beautifulcheck [tag]
+awesomecheck [tag]
+prettycheck [tag]
+lesbiancheck [tag]
+gaycheck [tag]
+cutecheck [tag]
+uglycheck [tag]
+hornycheck [tag]
+charactercheck [tag]
+cerhantu
+lovelycheck [tag]
+couple
+soulmate
+hot
+sexy
+kind
+idiot
+handsome
+beautiful
+cute
+pretty
+lesbian
+noob
+bastard
+foolish
+nerd
+asshole
+gay
+smart
+stubble
+dog
+horny
+cunt
+wibu
+noobra
+nibba
+nibbi
+comrade
+mumu
+rascal
+scumbag
+nuts
+fagot
+scoundrel
+ditch
+dope
+gucci
+lit
+dumbass
+crackhead
+mf
+motherfucker
+sucker
+fuckboy
+playboy
+fuckgirl
+playgirl
+quotes
+jalantikus
+infogempa
+weather
+myapikey
+cekapikey
+infokota
+hexcode
+cekresi
+alquran
+alquranaudio
+kisahnabi
+jadwalsholat
+asmaulhusna
+shortlink1
+shortlink2
+shortlink3
+shortlink4
+ouolink    
+nomorhoki
+artimimpi
+ramalanjodoh
+ramalanjodohbali
+ramalancinta
+ramalannasib
+suamiistri
+kecocokannama
+kecocokanpasangan
+artinama2
+jadianpernikahan
+sifatusaha
+pekerjaan
+rejeki
+penyakit
+tarot
+fengshui 
+haribaik
+harikeren
+harisial
+harinaga
+arahrejeki
+peruntungan
+wetonjawa
+sifat
+keberuntungan
+memancing
+masasubur
+zodiak
+shio
+ttsinglish
+ttsind
+ttsjpn    
+sewa 
+listsewa
+ceksewa
+setppgc2 panjang
+setppgc2
+setpppanjang 
+self
+addvn
+delvn
+listvn
+public
+join [link]
+leavegc
+setbio
+addprem
+delprem
+bctext [Rawan Banned]
+bcimage [Rawan Banned]
+bcvideo [RawanBanned]
+setbotpp [image]
+setthumb [reply img]
+setexif
+welcome on/off
+hijack
+block [tag/number]
+unblock [tag/number]
+setgcpp2 panjang
+colongpp
+grouplink
+listadmin
+closetime
+opentime
+grupsetting
+ephemeral [option]
+setgcpp [image]
+setname [text]
+setdesc [text]
+group 
+resetgrouplink
+editinfo [option]
+menfess [number]
+add [user]
+kick [reply/tag]
+hidetag [text]
+tagall [text]
+antilinkgc [on/off]
+antilinktg [on/off]
+antilinktt [on/off]
+antilinkytch [on/off]
+antilinkytvid [on/off]
+antilinkig [on/off]
+antilinkfb [on/off]
+antilinktwit [on/off]
+antilinkremove [on/off]
+antilinkdeletekick [on/off]
+antivirus [on/off]
+antitoxic [on/off]
+antiwame [on/off]
+nsfw [on/off]
+promote [reply/tag]
+demote [reply/tag]
+react [reply emoji]
+vote
+devote
+upvote
+checkvote
+delvote
+fotocp (New)
+tiktok [url]
+tiktokaudio [url]
+instagram [url]
+spotify [url]
+mediafire [url]
+ytmp3 [url|quality]
+ytmp4 [url|quality]
+play [query]
+gitclone [repo link]
+song [query]
+yts [query]
+lyrics [query]
+gimage [query]
+google [query]
+anime [query]
+pinterest [query]
+image [query]
+wallpaper [query]
+searchno [number]
+horoscope [query]
+imdb [movie name]
+weather [loc name]
+genshin [char name]
+wikimedia [query]
+ytsearch [query]
+ringtone [query]
+tourl[reply image/video]
+toqr [reply text]
+toimage [reply stick]
+sticker [reply img|gif]
+take [reply img|gif|stik]
+smeme [reply img]
+emoji [emoji]
+tovideo [reply img]
+togif [reply stick]
+tovn [reply aud]
+tomp3 [reply vn]
+toaudio [reply vid]
+ebinary [reply txt]
+dbinary [reply txt]
+tinyurl [link]
+styletext [text]
+volume [reply aud]
+bass [reply aud]
+blown [reply aud]
+deep [reply aud]
+earrape [reply aud]
+fast [reply aud]
+fat [reply aud]
+nightcore [reply aud]
+reverse [reply aud]
+robot [reply aud]
+slow [reply aud]
+smooth [reply aud]
+squirrel [reply aud]
+afk
+id
+toqr [link]
+repeat
+readmore [text]
+toviewonce
+fliptext [text]]
+chatinfo
+alive
+ping
+owner
+menu
+delete
+quoted
+listpc
+listgc
+donate
+chord
+ssweb
+myip
+request
+report [bug]
+removebg [reply img]
+tiktokgirl	
+tiktoknukhty
+tiktokpanrika
+tiktokkayes
+tiktoknotnot
+tiktokghea
+tiktoksantuy
+tiktokbocil
+instagramemoji
+facebookemoji
+iphoneemoji
+samsungemoji
+joyemoji
+skypeemoji
+twitteremoji
+whatsappemoji
+microsoftemoji
+googleemoji
+pediaemoji
+microsoftemoji
+art
+ktpmaker
+pornhub
+glitch
+avanger
+space
+ninjalogo
+wolflogo
+stel3d
+wallgravity 
+darkjoke
+memeindo
+randommeme
+tebak caklontong
+tebak lagu
+tebak gambar
+tebak kata
+tebakanime
+tebakanime
+tebakkabupaten
+tebakkimia
+tebakkalimat
+tebakbendera
+tebaklirik
+bts
+exo
+elf
+loli
+neko
+waifu
+shota
+husbu
+sagiri
+shinobi
+husbu
+shinobu
+mehumin
+wallanime                
+liststore
+deletemessage 
+updatelist
+addlist
+cekstatus
+setstatus
+bayar
+google < Query >
+gimage < Query >
+googleimage < Query >
+googleimage2 < Query >
+upscaler <reply image>
+jadianime <reply image>
+upscale <reply Image>
+remini <reply>
+arcane < img/reply>
+art < img/reply>
+comics < img/reply>
+stalknickaov
+stalknickchess
+stalkbigo
+stalkcocofun
+stalkig
+stalkgithub
+stalktwitter
+stalktiktok
+binary < Query >
+encodebinary < Query>
+ebinary < Query >
+dbinary < Query >
+codemorse < Query ><
+taugaksih
+faktaunik
+motivasi
+katabijak
+kanekilogo
+nekologo
+webultah
+webdeface
+websad
+pkifilter < reply img >
+circlefilter < reply img >
+brightfilter < replyimg >
+blurfilter < replyimg >
+cardfilter < replyimg >
+grayscale < replyimg >
+gayfilter < replyimg >
+contrasfilter < replyimg >
+xprofilter < replyimg >
+willowfilter < replyimg >
+nsfwechi
+nsfwechi2
+nsfwcum
+nsfwclasic
+nsfwbj1
+nsfwsusugede
+nsfwavatar
+nsfwthigh
+nsfwfetis
+nsfwahageo
+nsfwanal
+nsfwbooty
+nsfwarmpits
+bokeps (prem)
+gifhentai
+gifblowjob
+hentaivideo
+hneko
+nwaifu
+animespank
+trap
+gasm
+ahegao
+ass
+bdsm
+blowjob
+cuckold
+cum
+milf
+eba
+ero
+femdom
+foot
+gangbang
+glasses
+hentai
+jahy
+manga
+masturbation
+neko-hentai
+neko-hentai2
+nsfwloli
+orgy
+panties
+pussy
+tentacles
+thights
+yuri
+zettai
+eroholo
+eroyuri
+erokemo
+femdom2   
+addvn
+delvn
+listvn
+addvid
+delvid
+listvid
+addimg
+delimg
+listimg
+addvn
+delvn
+listvn
+addvid
+delvid
+listvid
+addimg
+delimg
+listimg
+aidraw < reply img>
+dalle < reply img>
+dalleanime < reply img>
+imgtohororv1 < reply img>
+imgtohororv2 < reply img>
+imgtohororv3 < reply img>
+jadianime < reply img>
+blurfilter < reply img>
+makequotes1
+makequotes2
+botcommand
+changemymind
+trumpcomment
+qc < reply text ~ text>
+badboyserti
+badgirlserti
+bucinserti
+fuckboyserti
+fuckgirlserti
+piagamserti
+textrunning <text>
+fakechat <reply>
+magernulis1v2 <text>
+magernulis2 <text>
+folio1 <text>
+hvs <text>
+`
+
+global.limitawal = {
+    premium: "Infinity",
+    free: 2,
+    monayawal: 1000
+}
+   global.rpg = {
+   darahawal: 100,
+   besiawal: 95,
+   goldawal: 30,
+   emeraldawal: 8,
+   umpanawal: 10,
+   potionawal: 5
+}
+global.limitawal = {
+    rakyat: "Infinity",
+    free: 100
+}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
